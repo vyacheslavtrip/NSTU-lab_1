cb163f64c99153c03f9ed1c6e4809cf65fdbb315
jyanadio <jyanadio@student.21-school.ru>
2025-03-10 10:36:59 +0700

    upd

--- vowelcount.cpp
+++ vowelcount.cpp
@@ -2,7 +2,7 @@
 
 int countVowels(const std::string& str) {
     int vowelCount = 0;
-    for (char c : str) {
+    for (char c : str) { /// ejkvnkejv
         if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
             vowelCount++;
         }
