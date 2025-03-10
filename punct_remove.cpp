8c97421058f8549ce93b4dedbfa7bb06e5caf136
mrpr78 <mrprety77@gmail.com>
2025-03-10 04:50:38 +0000

    added comment test

--- punct_remove.cpp
+++ punct_remove.cpp
@@ -2,7 +2,7 @@
 #include <cctype>
 
 std::string removePunctuation(const std::string& input) {
-    std::string result;//added comment for test
+    std::string result;//added comment for test 2
     for (char c : input) {
         if (std::isalnum(c) || c == ' ') { // Сохраняем буквы, цифры и пробелы
             result += c;
